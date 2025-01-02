a73ce0e3d00c02d79c1c3b76de0fe23596e999d2
Jonathan Kocevar <55568915+staticvalley@users.noreply.github.com>
2025-01-01 21:38:57 -0500

    [updt.] modified vec init impl. added concatenation and copy

--- headr_vector.h
+++ headr_vector.h
@@ -15,7 +15,7 @@
 
 #define HEADR_VECTOR_MIN_CAP 256
 
-/**
+/**_
  * @brief generates a vector container type for a given type
  * @param type_name the name to be set in the vector containers typedef, ie. headr_<type_name>_vec_t
  * @param base_type the given type to make a vector type for
@@ -25,14 +25,28 @@ typedef struct {\
 	base_type* data;\
 	size_t length;\
 	size_t capacity;\
+	size_t _type_size;\
 } headr_##type_name##_vec_t;\
 
 /**
  * @brief internally initializes a vector struct
  * @param v pointer to vector struct
+ * @param t the base type in the vector variable
+ * @note this is just a convienient wrapper for _headr_internal_vector_init
  */
-#define headr_vector_init(v) do {\
-       memset((v), 0, sizeof(*(v)));\
+#define headr_vector_init(v, t) do {\
+	_headr_internal_vector_init(v, sizeof(t));\
+} while (0)\
+
+
+/**
+ * @brief internal function that initializes a vector struct
+ * @param v pointer to vector struct
+ * @param s size of base type in bytes
+ */
+#define _headr_internal_vector_init(v, s) do {\
+	memset((v), 0, sizeof(*(v)));\
+	(v)->_type_size = s;\
 } while (0)\
 
 /**
@@ -41,7 +55,8 @@ typedef struct {\
  */
 #define headr_vector_free(v) do {\
 	if((v)->data != NULL) { free((v)->data); }\
-	headr_vector_init(v);\
+	size_t headr_internal_vector_type_size = (v)->_type_size;\
+	_headr_internal_vector_init(v, headr_internal_vector_type_size);\
 } while(0)\
 
 /**
@@ -50,10 +65,10 @@ typedef struct {\
  * @param e element to appent to vector
  */
 #define headr_vector_append(v, e) do {\
-	if((v)->length == (v)->capacity) {\
+	if((v)->length >= (v)->capacity) {\
 		if((v)->capacity == 0) {(v)->capacity = HEADR_VECTOR_MIN_CAP;}\
 		else {(v)->capacity *= 2;}\
-		_headr_vector_resize(v);\
+		_headr_internal_vector_resize(v);\
 	}\
 	(v)->data[(v)->length] = e;\
 	(v)->length++;\
@@ -68,7 +83,7 @@ typedef struct {\
 	size_t headr_internal_vector_reserve_diff = (v)->capacity - (v)->length;\
 	if(headr_internal_vector_reserve_diff < n) {\
 		(v)->capacity += n - headr_internal_vector_reserve_diff;\
-		_headr_vector_resize(v);\
+		_headr_internal_vector_resize(v);\
 	}\
 } while(0)\
 
@@ -76,11 +91,11 @@ typedef struct {\
  * @brief internal function that reallocates memory based on a vectors current capacity
  * @param v pointer to vector struct
  */
-#define _headr_vector_resize(v) do {\
-	void* headr_internal_vector_data_ptr = realloc((v)->data, sizeof((v)->data) * (v)->capacity);\
+#define _headr_internal_vector_resize(v) do {\
+	void* headr_internal_vector_data_ptr = realloc((v)->data, (v)->_type_size * (v)->capacity);\
 	if(headr_internal_vector_data_ptr == NULL) {\
 		headr_vector_free(v);\
-		fprintf(stderr, "headr: error: realloc failed\n");\
+		fprintf(stderr, "headr_vector.h: line %d: error: realloc failed\n", __LINE__);\
 		exit(EXIT_FAILURE);\
 	}\
 	(v)->data = headr_internal_vector_data_ptr;\
@@ -92,8 +107,30 @@ typedef struct {\
  * @param i index to element to remove
  */
 #define headr_vector_remove(v, i) do {\
-	for(int j = i; j < (v)->length - 1; j++) { (v)->data[j] = (v)->data[j+1]; } \
-	(v)->length -= 1;\
+	if(i < ((v)->length)) { (v)->length--; }\
+	for(int j = i; j < (v)->length; j++) { (v)->data[j] = (v)->data[j+1]; } \
+} while(0)\
+
+/**
+ * @brief concatenates elements of one vector to another
+ * @param vd pointer to the destination vector struct to concat to
+ * @param vs pointer to the source vector struct to copy from
+ */
+#define headr_vector_concat(vd, vs) do {\
+	headr_vector_reserve(vd, (vs)->length);\
+	memcpy(((vd)->data + (vd)->length), (vs)->data, (vs)->length * (vs)->_type_size);\
+	(vd)->length += (vs)->length;\
+} while(0)\
+
+/**
+ * @brief copies the data from one vector to another
+ * @param vd pointer to the destination vector struct to copy to
+ * @param vs pointer to the source vector struct to copy from
+ * @note this operation is destructive, and will not free any data in vd before copying
+ */
+#define headr_vector_copy(vd, vs) do {\
+	_headr_internal_vector_init(vd, (vs)->_type_size);\
+	headr_vector_concat(vd, vs);\
 } while(0)\
 
 /**
@@ -104,7 +141,7 @@ headr_vector_generate_type(char, char)
 headr_vector_generate_type(float, float)
 headr_vector_generate_type(double, double)
 headr_vector_generate_type(str, char*)
-headr_vector_generate_type(voidptr, void*)
+headr_vector_generate_type(void, void*)
 
 #endif
 
